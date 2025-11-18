b64c6dcb03033f4710e36f3c52f5dfb19c5a24b1
mitchodvadiya <mitchodavadiya@gmail.com>
2025-11-18 17:09:42 +0530

    Refactor ffmpeg command for audio conversion

--- bhajan.py
+++ bhajan.py
@@ -3,8 +3,9 @@ import ffmpeg
 input_file = r"D:\python programming\python_project\bhajan.mp4"
 output_file = 'convert.mp3'
 
-ffmpeg.input(input_file).output(output_file, format='mp3', acodec='libmp3lame').run()
+ffmpeg.run( ffmpeg.input(input_file).output(output_file))
 
 print("Audio saved as MP3!")
+
 
                                             
